d844527597abdfeaec73ca825df3fc5b814258f8
lauroro <88981092+lauroro@users.noreply.github.com>
2022-07-26 11:34:03 +0200

    fix javascript lsp

--- init.lua
+++ init.lua
@@ -165,3 +165,8 @@ for _, server in ipairs(lsp_installer.get_installed_servers()) do
     }
   end
 end
+require'lspconfig'.tsserver.setup{
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  root_dir = function() return vim.loop.cwd() end      -- run lsp for javascript in any directory
+
+}
